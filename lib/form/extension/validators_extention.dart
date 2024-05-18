37b5ce826d84ace669b7020ed23780548bf6da28
AritroSinha <sinhaaritro@gmail.com>
2024-05-18 08:28:38 +0530

    Fixed extension on class

--- lib/form/extension/validators_extention.dart
+++ lib/form/extension/validators_extention.dart
@@ -1,7 +1,7 @@
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:test_reactive_forms/form/unique_email_async_validator.dart';
 
-extension CustomValidators on Validator {
+extension CustomValidators on Validators {
   /// Creates a validator that checks is the email is valid in the server
   static AsyncValidator<dynamic> get uniqueAsyncEmail =>
       UniqueEmailAsyncValidator();
